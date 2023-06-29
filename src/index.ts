39b9805d1195924fd2e46b9d5fbcf4db793055b1
Devonte Duncan <devonteduncan@macbook-pro.mynetworksettings.com>
2023-06-29 15:09:36 -0400

    change default port

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import 'dotenv/config';
 import database from './database/dbPool';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
