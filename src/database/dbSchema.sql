23fe4405b870c2d841b692663dd2e1bcaf365a25
Devonte Duncan <devonteduncan@macbook-pro.mynetworksettings.com>
2023-07-09 18:01:04 -0400

    Edit and finalize schema and seed data

--- src/database/dbSchema.sql
+++ src/database/dbSchema.sql
@@ -6,6 +6,15 @@ CREATE SCHEMA public;
 GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO public;
 
+
+DROP TABLE IF EXISTS categories;
+CREATE TABLE categories (
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  name VARCHAR(255) NOT NULL UNIQUE,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
 --
 -- Creates a table for employee users belonging to business entities
 --
@@ -18,11 +27,36 @@ CREATE TABLE users(
    phone_number char(11) NOT NULL,
    is_creator bool NOT NULL,
    is_active bool NOT NULL,
+   profile_category uuid REFERENCES categories(id),
    profile_image_url VARCHAR,
-   saved_categories VARCHAR[],
-   profile_category VARCHAR,
-   portfolio_images VARCHAR[],
    price_range VARCHAR(5),
    password VARCHAR(100) NOT NULL,
-   joined_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+   created_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+DROP TABLE IF EXISTS posts;
+CREATE TABLE posts(
+   id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+   user_id uuid REFERENCES users(id),
+   caption VARCHAR(120),
+   profile_category uuid REFERENCES categories(id),
+   image_url VARCHAR,
+   created_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+DROP TABLE IF EXISTS comments;
+CREATE TABLE comments(
+   id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+   user_id uuid REFERENCES users(id),
+   post_id uuid REFERENCES posts(id),
+   parent_comment_id uuid,
+   body VARCHAR(120) NOT NULL,
+   created_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+DROP TABLE IF EXISTS user_favorite_categories;
+CREATE TABLE user_favorite_categories (
+  user_id uuid REFERENCES users(id),
+  category_id uuid REFERENCES categories(id),
+  PRIMARY KEY (user_id, category_id)
 );
--- src/database/dbSeedData.sql
+++ src/database/dbSeedData.sql
@@ -1,5 +1,31 @@
-INSERT INTO users (first_name, last_name, email, phone_number, is_creator, is_active, profile_image_url, saved_categories, profile_category, portfolio_images, price_range, password)
-    VALUES ('Devonte', 'Duncan', 'devonteduncan202@gmail.com', '13475554333', True, True, 'https://avatars.githubusercontent.com/u/14931357?v=4', '{"photography", "graphic_design"}', 'software_development', '{"https://www.searchenginejournal.com/wp-content/uploads/2019/10/ontrack-home-page-5dbad77d77c54.jpg", "https://cdn2.hubspot.net/hubfs/53/mint-homepage-design.png"}', '$$$$', '1234');
+-- Inserting sample data into the "categories" table
+INSERT INTO categories (name)
+VALUES
+  ('Nature'),
+  ('Abstract'),
+  ('Portrait');
 
-INSERT INTO users (first_name, last_name, email, phone_number, is_creator, is_active, profile_image_url, saved_categories, profile_category, portfolio_images, price_range, password)
-    VALUES ('Shemar', 'Gordon', 'shemar.gordon32@gmail.com', '13473352133', True, True, 'https://avatars.githubusercontent.com/u/14931374?v=4', '{"ux_design", "graphic_design"}', 'software_development', '{"https://www.searchenginejournal.com/wp-content/uploads/2019/10/ontrack-home-page-5dbad77d77c54.jpg", "https://cdn2.hubspot.net/hubfs/53/mint-homepage-design.png"}', '$$$$', '1234');
+-- Inserting sample data into the "users" table
+INSERT INTO users (first_name, last_name, email, phone_number, is_creator, is_active, profile_image_url, profile_category, price_range, password)
+VALUES
+  ('John', 'Doe', 'johndoe@example.com', '1234567890', true, true, 'https://example.com/profile.jpg', (SELECT id FROM categories WHERE name = 'Nature'), '$$', 'password123'),
+  ('Jane', 'Smith', 'janesmith@example.com', '9876543210', true, true, 'https://example.com/profile.jpg', (SELECT id FROM categories WHERE name = 'Abstract'), '$$$', 'password456');
+
+-- Inserting sample data into the "posts" table
+INSERT INTO posts (user_id, caption, image_url)
+VALUES
+  ((SELECT id FROM users WHERE email = 'johndoe@example.com'), 'Beautiful landscape', 'https://example.com/images/landscape.jpg'),
+  ((SELECT id FROM users WHERE email = 'janesmith@example.com'), 'Amazing artwork', 'https://example.com/images/artwork.jpg');
+
+-- Inserting sample data into the "user_favorite_categories" table
+INSERT INTO user_favorite_categories (user_id, category_id)
+VALUES
+  ((SELECT id FROM users WHERE email = 'johndoe@example.com'), (SELECT id FROM categories WHERE name = 'Nature')),
+  ((SELECT id FROM users WHERE email = 'janesmith@example.com'), (SELECT id FROM categories WHERE name = 'Abstract'));
+
+-- Inserting sample data into the "comments" table
+INSERT INTO comments (user_id, post_id, parent_comment_id, body)
+VALUES
+  ((SELECT id FROM users WHERE email = 'johndoe@example.com'), (SELECT id FROM posts WHERE caption = 'Beautiful landscape'), NULL, 'Great shot!'),
+  ((SELECT id FROM users WHERE email = 'janesmith@example.com'), (SELECT id FROM posts WHERE caption = 'Amazing artwork'), NULL, 'I love the colors!'),
+  ((SELECT id FROM users WHERE email = 'johndoe@example.com'), (SELECT id FROM posts WHERE caption = 'Amazing artwork'), (SELECT id FROM comments WHERE body = 'I love the colors!'), 'Thank you!');
